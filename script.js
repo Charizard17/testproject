c96762c5c8def5487813b7b8b27bea9d177a533e
Charizard17 <3723esad@gmail.com>
2020-11-23 16:26:23 +0100

    changes made

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-document.getElementById("test").innerHTML = "This is a text from script.js";
+test = document.getElementById("test");
+
+test.innerHTML = "This is a text from script.js";
+test.style.color = "red";
